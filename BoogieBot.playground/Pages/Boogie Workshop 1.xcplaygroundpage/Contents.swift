a8b4d41996de37b0636a107c6ea9f8cad17f6e8c
brozozowska <s_rozov@me.com>
2023-03-18 13:01:42 +0300

    Initial Commit

---
+++ BoogieBot.playground/Pages/Boogie Workshop 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+## Boogie Workshop
+ 
+ This page is here for you to create your own routines. 
+ 
+ Remember the moves:
+ 
+- `leftArmUp()`, `leftArmDown()`, `rightArmUp()`, `rightArmDown()`
+- `leftLegUp()`, `leftLegDown()`, `rightLegUp()`, `rightLegDown()`
+- `shakeItLeft()`, `shakeItRight()`, `shakeItCenter()`
+- `jumpUp()`, `jumpDown()`
+- `fabulize()`, `defabulize()`
+ 
+ To sign your work:
+ 
+ `setBotTitle("My Awesome Dance")`\
+ `setBotSubtitle("By The Boogiemaster")`
+ */
+startBot()
+startRecording()
+setBotTitle("Awesome dance party!")
+setBotSubtitle("By The Boogiemaster")
+func discoDanceMoves() {
+    leftArmUp()
+    shakeItRight()
+    leftArmDown()
+    shakeItCenter()
+    jumpUp()
+    jumpDown()
+    leftArmUp()
+    shakeItRight()
+    leftArmDown()
+    shakeItCenter()
+    jumpUp()
+    jumpDown()
+}
+fabulize()
+discoDanceMoves()
+defabulize()
+/*:
+Continue with your creativity.
+
+[Previous](@previous)  |  page 10 of 13  |  [Next: Boogie Workshop](@next)
+ */
